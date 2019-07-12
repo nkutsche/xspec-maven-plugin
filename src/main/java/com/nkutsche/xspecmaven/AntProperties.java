9ad326f917e932cc91f02a896beb439c1c2fb35c
Nico Kutscherauer <kutscherauer@data2type.de>
2019-07-12 12:14:53 +0200

    Refactor and restructures properties/parameters, fixes includes/excludes for XSpec search, adds testType parameter

---
+++ src/main/java/com/nkutsche/xspecmaven/AntProperties.java
@@ -0,0 +1,30 @@
+package com.nkutsche.xspecmaven;
+
+public interface AntProperties {
+
+    //    SOURCE
+    String PROJECT_DIR = "project.dir";
+    String TEST_DIR = "test.dir";
+    String CATALOG = "catalog";
+    String INCLUDES = "includes";
+    String EXCLUDES = "excludes";
+
+    //    Configuration
+    String TEST_TYPE = "test.type";
+
+    //    PROCESS FILES
+    String ANT_FILE = "ant.file";
+    String XSPEC_PACKAGE = "xspec.package";
+    String XSPEC_FRAMEWORK = "xspec.framework";
+    String BUILD_DIR = "build.dir";
+    String WORKING_DIR = "working.dir";
+    String TEMP_DIR = "temp.dir";
+    String CLASS_PATH = "java.class.path";
+
+
+
+    //    OUTPUT
+    String REPORT_DIR = "report.dir";
+    String GENERATE_SUREFIRE_REPORTS = "generateSurefireReport";
+    String SUREFIRE_REPORT_DIR = "surefireReportDir";
+}
--- src/main/java/com/nkutsche/xspecmaven/XSpecMojo.java
+++ src/main/java/com/nkutsche/xspecmaven/XSpecMojo.java
@@ -9,35 +9,41 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 import org.apache.tools.ant.*;
-import org.apache.tools.ant.launch.Launcher;
 import org.apache.commons.io.IOUtils;
 import org.codehaus.plexus.classworlds.realm.ClassRealm;
 
 import java.io.*;
-import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.List;
+import java.util.Properties;
+
+import static com.nkutsche.xspecmaven.AntProperties.*;
 
 @Mojo(name = "run", defaultPhase = LifecyclePhase.TEST)
 public class XSpecMojo extends AbstractMojo {
 
-    @Parameter(defaultValue = "${project.build.directory}/xspecmaven")
-    private File tempFolder;
+    @Parameter(defaultValue = "${project.build.directory}/xspecmaven", property = "com.nkutsche.xspecmaven.workingDir")
+    private File workingDir;
+
+    @Parameter(defaultValue = "${com.nkutsche.xspecmaven.workingDir}/xspec-temp-files")
+    private File xspecTempDir;
 
     @Parameter(defaultValue = "${project.basedir}/src/test/xspec/schematron")
-    private File xspecFolder;
+    private File testDir;
 
     @Parameter(defaultValue = "**/*.xspec")
     private String[] includes;
 
-    @Parameter(alias = "excludes")
-    public List<String> excludes;
+    @Parameter(alias = "excludes", defaultValue = "**/*.xspec-compiled.xspec")
+    public String[] excludes;
 
     @Parameter(defaultValue = "${catalog.filename}")
-    public String catalogFile;
+    public File catalogFile;
 
     @Parameter(defaultValue = "${project.build.directory}/surefire-reports")
     public File surefireReportDir;
+
+    @Parameter(defaultValue = "${project.build.directory}", readonly = true)
+    public File mvnBuildDir;
 
     @Parameter(defaultValue = "false")
     public Boolean generateSurefireReport;
@@ -51,23 +57,27 @@ public class XSpecMojo extends AbstractMojo {
     @Parameter( defaultValue = "${plugin}", readonly = true )
     private PluginDescriptor pluginDescriptor;
 
+    private enum TestType {
+        SCHEMATRON, XSLT, XQUERY
+    }
+    @Parameter(defaultValue = "SCHEMATRON")
+    private TestType testType;
+
     public void execute() throws MojoExecutionException, MojoFailureException {
 
         getLog().info("Start XSpec for Maven and Schematron");
 
 
-        for (String incl:
-             this.includes) {
-            getLog().info(incl);
-        }
 
 
         try {
-            extractResources();
+            Properties antProperties = extractResources(initProperties());
+
 
             Project p = new Project();
             p.addBuildListener(new BuildListener() {
                 public void buildStarted(BuildEvent buildEvent) {
+
                 }
 
                 public void buildFinished(BuildEvent buildEvent) {
@@ -75,7 +85,7 @@ public class XSpecMojo extends AbstractMojo {
                 }
 
                 public void targetStarted(BuildEvent buildEvent) {
-
+                    messageLogged(buildEvent);
                 }
 
                 public void targetFinished(BuildEvent buildEvent) {
@@ -83,7 +93,7 @@ public class XSpecMojo extends AbstractMojo {
                 }
 
                 public void taskStarted(BuildEvent buildEvent) {
-
+                    messageLogged(buildEvent);
                 }
 
                 public void taskFinished(BuildEvent buildEvent) {
@@ -110,17 +120,32 @@ public class XSpecMojo extends AbstractMojo {
                     }
                 }
             });
-//            p.setCoreLoader(verfiyDependencies());
-            ProjectHelper.configureProject(p, new File(tempFolder, "build.xml"));
-            p.setUserProperty("xspec.dir", xspecFolder.getAbsolutePath());
-            p.setUserProperty("project.dir", projectBaseDir.getAbsolutePath());
-            p.setUserProperty("java.class.path", verfiyDependencies());
-            getLog().info("Start ant...");
+            ProjectHelper.configureProject(
+                    addPropertiesToAntProject(p, antProperties),
+                    new File(antProperties.getProperty(ANT_FILE)));
+
+
+
+
+
+            getLog().info("Start nested ANT process...");
             p.executeTarget(p.getDefaultTarget());
         } catch (IOException e) {
             e.printStackTrace();
-            throw new MojoExecutionException("Execution failed because of fatal error", e);
+            throw new MojoExecutionException("Execution failed because of a fatal error", e);
         }
+    }
+
+    private Project addPropertiesToAntProject(Project project, Properties properties){
+
+        for (Object keyObj:
+             properties.keySet()) {
+            String key = keyObj.toString();
+            project.setUserProperty(key, properties.getProperty(key));
+        }
+
+        return project;
+
     }
 
     private String verfiyDependencies() throws MojoExecutionException {
@@ -142,30 +167,93 @@ public class XSpecMojo extends AbstractMojo {
         return classPath;
     }
 
+    private Properties initProperties() throws MojoExecutionException {
+        Properties properties = new Properties();
 
-    private void extractResources() throws IOException, MojoExecutionException {
 
-        getLog().debug("Tempfolder: " + tempFolder.toString());
+        properties.setProperty(PROJECT_DIR, projectBaseDir.getAbsolutePath());
+        properties.setProperty(TEST_DIR, testDir.getAbsolutePath());
+        if(catalogFile != null)
+            properties.setProperty(CATALOG, catalogFile.getAbsolutePath());
 
-        tempFolder.mkdirs();
+        properties.setProperty(INCLUDES, filelistToString(this.includes));
+
+        properties.setProperty(EXCLUDES, filelistToString(this.excludes));
+
+        String testTypeValue;
+        switch (testType){
+            case XSLT:
+                testTypeValue = "t";
+                break;
+            case XQUERY:
+                testTypeValue = "q";
+                break;
+            case SCHEMATRON:
+            default:
+                testTypeValue = "s";
+                break;
+        }
+        properties.setProperty(TEST_TYPE, testTypeValue);
+
+
+        properties.setProperty(ANT_FILE, new File(workingDir, "build.xml").getAbsolutePath());
+        properties.setProperty(XSPEC_PACKAGE, new File(workingDir, "xspec-1.3.0.zip").getAbsolutePath());
+
+        properties.setProperty(XSPEC_FRAMEWORK, new File(workingDir, "xspec-framework/xspec-1.3.0").getAbsolutePath());
+
+        properties.setProperty(BUILD_DIR, mvnBuildDir.getAbsolutePath());
+        properties.setProperty(WORKING_DIR, workingDir.getAbsolutePath());
+        properties.setProperty(TEMP_DIR, xspecTempDir.getAbsolutePath());
+        properties.setProperty(CLASS_PATH, verfiyDependencies());
+
+        properties.setProperty(REPORT_DIR, new File(workingDir, "xspec-reports").getAbsolutePath());
+
+        if(generateSurefireReport){
+            properties.setProperty(GENERATE_SUREFIRE_REPORTS, "true");
+            properties.setProperty(SUREFIRE_REPORT_DIR, surefireReportDir.getAbsolutePath());
+        }
+
+        return properties;
+    }
+
+
+    private Properties extractResources(Properties properties) throws IOException, MojoExecutionException {
+
+        getLog().debug("Tempfolder: " + workingDir.toString());
+
+        workingDir.mkdirs();
 
         getLog().debug("Created temp folder successfully!");
 
-        extractResources("/build.xml", "build.xml");
-        extractResources("/xspec-1.3.0.zip", "xspec-1.3.0.zip");
+        extractResources("/build.xml", new File(properties.getProperty(ANT_FILE)));
+        extractResources("/xspec-1.3.0.zip", new File(properties.getProperty(XSPEC_PACKAGE)));
+
+        return properties;
 
     }
 
-    private void extractResources(String src, String out) throws IOException, MojoExecutionException {
+    private String extractResources(String src, File out) throws IOException, MojoExecutionException {
         getLog().debug("Extract " + src + " to " + out);
 
         InputStream stream = XSpecMojo.class.getResourceAsStream(src);
         if(stream == null){
             throw new MojoExecutionException("Could not find source " + src);
         }
-        FileOutputStream outputStream = new FileOutputStream(new File(tempFolder, out));
+
+        FileOutputStream outputStream = new FileOutputStream(out);
         IOUtils.copy(stream, outputStream);
 
         getLog().debug("Extraction of " + src + " successufll!");
+
+        return out.getAbsolutePath();
+    }
+
+    private String filelistToString(String[] files){
+        String result = "";
+        for (String incl:
+                files) {
+            result += incl + ",";
+        }
+        return result;
     }
 }
